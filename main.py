75f8429c0dba59ad577bcd5f4d4e6f7cb0c8ee80
Rutvik Kapse <kapse.rutvik04@gmail.com>
2023-09-16 21:53:20 +0530

    Removed bug for input roman number "IIII".
    Removed the bug which was converting invalid number "IIII" to 4(IV) and similar numbers. eg. "XVIIII" to 19(XIX).

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 # This is a program to convert roman numbers to decimal.
-values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-print(values)
 
-
-def RomanToDecimal(s):
-    n = len(s)
-    sum = 0
+def RomanToDecimal(roman):
+    n = len(roman)
+    decimal = 0
     i = 0
 
     while (i < n):
-        if (i != n-1 and values[s[i]] < values[s[i+1]]):
-            sum += values[s[i+1]] - values[s[i]]
+        if (i != n-1 and values[roman[i]] < values[roman[i+1]]):
+            decimal += values[roman[i+1]] - values[roman[i]]
             i += 2
         else:
-            sum += values[s[i]]
+            decimal += values[roman[i]]
             i += 1
-    return sum
+    return decimal
 
 
 if __name__ == "__main__":
+    values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman = input("Enter roman Number: ").upper()
+
+    while ("IIII" in roman):
+        print("Invalid roman number.")
+        roman = input("Enter roman Number: ").upper()
+
+    print(values)
     print(f"Decimal value of {roman} number is", RomanToDecimal(roman))
